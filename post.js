71375e95d3feac6d58f37447003e5f8dc12501bb
PesquisaAdoro <102701432+PesquisaAdoro@users.noreply.github.com>
2022-06-24 12:10:13 -0300

    Update post.js

--- post.js
+++ post.js
@@ -16,7 +16,8 @@ function fazPost(url, body) {
 
 function produto1() {
     event.preventDefault()
-    let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    //let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    let url = "https://sheetdb.io/api/v1/zupdfe4hb4eu1"
 
     let data_atual = document.getElementById("data_atual").value
     let pdv = document.getElementById("pdv").value
@@ -43,7 +44,8 @@ function produto1() {
 
 function produto2() {
     event.preventDefault()
-    let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    //let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    let url = "https://sheetdb.io/api/v1/zupdfe4hb4eu1"
 
     let data_atual = document.getElementById("data_atual").value
     let pdv = document.getElementById("pdv").value
@@ -70,7 +72,8 @@ function produto2() {
 
 function produto3() {
     event.preventDefault()
-    let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    //let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    let url = "https://sheetdb.io/api/v1/zupdfe4hb4eu1"
 
     let data_atual = document.getElementById("data_atual").value
     let pdv = document.getElementById("pdv").value
@@ -97,7 +100,8 @@ function produto3() {
 
 function produto4() {
     event.preventDefault()
-    let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    //let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    let url = "https://sheetdb.io/api/v1/zupdfe4hb4eu1"
 
     let data_atual = document.getElementById("data_atual").value
     let pdv = document.getElementById("pdv").value
@@ -124,7 +128,8 @@ function produto4() {
 
 function produto5() {
     event.preventDefault()
-    let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    //let url = "https://sheetdb.io/api/v1/g2qtlyij9wgih"
+    let url = "https://sheetdb.io/api/v1/zupdfe4hb4eu1"
 
     let data_atual = document.getElementById("data_atual").value
     let pdv = document.getElementById("pdv").value
